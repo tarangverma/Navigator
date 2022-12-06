27e0da477ef4163b6c178ac487032a5e6093f0f4
Tarang Verma <tarangverma004@gmail.com>
2022-12-06 12:14:06 +0530

    Added markers

--- backend/routes/Pins.js
+++ backend/routes/Pins.js
@@ -19,7 +19,7 @@ router.get("/" , async (req,res) => {
       const pins = await Pin.find();
       res.status(200).json(pins) 
     } catch (err) {
-        
+      res.status(500).json(err);
     }
 })
 
--- frontend/navitor/src/App.js
+++ frontend/navitor/src/App.js
@@ -4,12 +4,27 @@ import Map , {Marker , Popup} from 'react-map-gl';
 import "./App.css";
 import StarRateIcon from '@mui/icons-material/StarRate';
 import PlaceIcon from '@mui/icons-material/Place';
+import axios from "axios";
 
 const MY_ACCESS_TOKEN = "pk.eyJ1IjoidGFyYW5nMDA0IiwiYSI6ImNsYjZjemN6ejAzaXozbm9icG94bnRyeHIifQ.JuvydUYsrlj-QgH_HN27Sw"
 function App() {
   const [showPopup, setShowPopup] = React.useState(true);
-  return (
-    <Map
+  const [pins , setPins] = React.useState([])
+
+  React.useEffect(() => {
+    const getPins = async () =>{
+     try {
+      const res = await axios.get("/pins");
+      setPins(res.data)  
+     } catch (err) {
+      console.log(err);
+     }
+    };
+    getPins();
+    }, []);
+    
+    return (
+      <Map
       initialViewState={{
         longitude: 88.3639,
         latitude: 22.5726,
@@ -18,17 +33,39 @@ function App() {
       style={{width: "100vw", height: "100vh"}}
       mapStyle="mapbox://styles/mapbox/streets-v9"
       mapboxAccessToken={MY_ACCESS_TOKEN} >
-      <Marker longitude={88.3639} latitude={22.5726} anchor="bottom" >
-      <PlaceIcon />
-    </Marker>
-    {showPopup && (
-      <Popup longitude={88.3639} latitude={22.5726}
-        anchor="left"
-        onClose={() => setShowPopup(false)}>
-        You are here
-      </Popup>)}
+       
+     {pins.map(p=> (
+<>
+       <Marker longitude={p.lon} latitude={p.lat} anchor="bottom" >
+       <PlaceIcon />
+       </Marker>
+     { showPopup && (
+      <Popup longitude={p.lon} latitude={p.lat}
+      anchor="left"
+      onClose={() => setShowPopup(true)}>
+        <div className='card'>
+          <label>Place</label> 
+        <h4 className='place'>{p.title}</h4>
+        <label>Review</label>
+        <p className='desc'>{p.desc}</p>
+        <label>Rating</label>
+        <div className='stars'>  
+        <StarRateIcon className='star' />
+        <StarRateIcon className='star' />
+        <StarRateIcon className='star' />
+        <StarRateIcon className='star' />
+        <StarRateIcon className='star' />
+        </div>
+        <label>Information</label>
+        <span className='username'>Created by <b>{p.username}</b></span>
+        <span className='date'>1 hour ago</span>
+        </div>
+      </Popup>
+      )}</>
+     ))}  
     </Map>
   );
+
 }
 
 export default App;
